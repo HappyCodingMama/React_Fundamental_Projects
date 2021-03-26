df93fe012dd4c50a23fc1a4353542d4e510dc44b
HappyCodingMama <happycodingmama@gmail.com>
2021-03-26 21:52:39 +0900

    Update data.js

--- reviews/src/data.js
+++ reviews/src/data.js
@@ -3,7 +3,9 @@ const reviews = [
     id: 1,
     name: "susan smith",
     job: "web developer",
-    image: "",
-    text: "I'm baby meggings",
+    image:
+      "https://cdn.abcotvs.com/dip/images/6213148_052620-cc-ap-spacex-dragon-img.jpg?w=1600",
+    text:
+      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up.",
   },
 ];
