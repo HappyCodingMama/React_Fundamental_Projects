7e204cce31f3120a7de6a4b100be1edbdfe846bc
HappyCodingMama <happycodingmama@gmail.com>
2021-03-28 21:32:58 +0900

    update Review

--- reviews/src/Review.js
+++ reviews/src/Review.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FachevronRight, FaQuoteRight } from "react-icons/fa";
 
 const review = () => {
+  const [index, setIndex] = useState(0);
   return (
     <div>
       <h2>Review Component</h2>
--- reviews/src/data.js
+++ reviews/src/data.js
@@ -8,4 +8,15 @@ const reviews = [
     text:
       "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up.",
   },
+  {
+    id: 2,
+    name: "magarette church",
+    job: "graphic designer",
+    image:
+      "https://cdn.abcotvs.com/dip/images/6213148_052620-cc-ap-spacex-dragon-img.jpg?w=1600",
+    text:
+      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up.",
+  },
 ];
+
+export default reviews;
