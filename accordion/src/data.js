92ea7ad89482e439ec6fea664a1cad901099a299
HappyCodingMama <happycodingmama@gmail.com>
2021-04-05 21:02:06 +0900

    download icons

--- accordion/src/data.js
+++ accordion/src/data.js
@@ -1,12 +1,14 @@
 const questions = [
   {
     id: 1,
-    title: 'Do I have to allow the use of cookies?',
-    info: 'Unicorn vinyl poutine brooklyn, next level direct trade iceland.',
+    title: "Do I have to allow the use of cookies?",
+    info: "Unicorn vinyl poutine brooklyn, next level direct trade iceland.",
   },
   {
     id: 2,
-    title: 'How do I cance my page password?',
-    info: 'Unicorn vinyl poutine brooklyn, next level direct trade iceland.',
+    title: "How do I cance my page password?",
+    info: "Unicorn vinyl poutine brooklyn, next level direct trade iceland.",
   },
 ];
+
+export default questions;
