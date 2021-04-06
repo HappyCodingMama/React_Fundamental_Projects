f519c1100ab206171470b540b5091c769e078a57
HappyCodingMama <happycodingmama@gmail.com>
2021-04-06 22:17:06 +0900

    add question component

---
+++ accordion/src/Question.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
+const Question = () => {
+  return <h2>Question component</h2>;
+};
+
+export default Question;
