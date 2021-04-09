608525a687bf9fa51e680cb388b383c20100ef32
HappyCodingMama <happycodingmama@gmail.com>
2021-04-10 08:40:57 +0900

    revise App.js

---
+++ 005menudisplay/src/data.js
@@ -0,0 +1,34 @@
+const menu = [
+  {
+    id: 1,
+    tuitle: "buttermilk pancakes",
+    category: "breakfast",
+    price: 15.99,
+    img:
+      "https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Savory-Pork-Salad_EXPS_CF2BZ19_35244_E12_13_2b.jpg",
+    desc:
+      "I'm baby woke milkshake wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
+  },
+  {
+    id: 2,
+    tuitle: "buttermilk pancakes",
+    category: "breakfast",
+    price: 15.99,
+    img:
+      "https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Savory-Pork-Salad_EXPS_CF2BZ19_35244_E12_13_2b.jpg",
+    desc:
+      "I'm baby woke milkshake wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
+  },
+  {
+    id: 3,
+    tuitle: "buttermilk pancakes",
+    category: "breakfast",
+    price: 15.99,
+    img:
+      "https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Savory-Pork-Salad_EXPS_CF2BZ19_35244_E12_13_2b.jpg",
+    desc:
+      "I'm baby woke milkshake wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
+  },
+];
+
+export default menu;
