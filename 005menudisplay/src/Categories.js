775a0ca766c6e50f0c63e262c4ec949de623f447
HappyCodingMama <happycodingmama@gmail.com>
2021-04-13 20:57:24 +0900

    add filter

---
+++ 005menudisplay/src/Categories.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Categories = () => {
+  return (
+    <div className="btn-container">
+      <button className="filter-btn" onClick={() => filterItems("breakfast")}>
+        breakfast
+      </button>
+    </div>
+  );
+};
+
+export default Categories;
