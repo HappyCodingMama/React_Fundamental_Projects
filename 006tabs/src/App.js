c0710ce02d3090f318dd939b2ab3922aba843a0f
HappyCodingMama <happycodingmama@gmail.com>
2021-04-19 20:38:57 +0900

    Update App.js

--- 006tabs/src/App.js
+++ 006tabs/src/App.js
@@ -3,6 +3,15 @@ import React, { useState, useEffect } from "react";
 const url = "https://course-api.netlify.app/api/react-tabs-project";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+  };
+
   return <h2>Tabs Project Setup</h2>;
 }
 
