c66d4952357f3e5a5496ed77144d08700c280a76
HappyCodingMama <happycodingmama@gmail.com>
2021-04-29 22:46:47 +0900

    update data.js

---
+++ 007reviews/src/data.js
@@ -0,0 +1,39 @@
+const people = [
+  {
+    id: 1,
+    image:
+      "https://www.memphisveterinaryspecialists.com/files/best-breeds-of-house-cats-memphis-vet-1-1.jpeg",
+    name: "maria ferguson",
+    title: "office manager",
+    quote:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel lacinia felis. Vivamus vestibulum, libero a placerat semper, nisi tortor dictum velit, egestas iaculis felis libero quis nulla. ",
+  },
+  {
+    id: 2,
+    image: "https://www.orlandocatcafe.com/wp-content/uploads/2020/07/12.png",
+    name: "john doe",
+    title: "regular guy",
+    quote:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel lacinia felis. Vivamus vestibulum, libero a placerat semper, nisi tortor dictum velit, egestas iaculis felis libero quis nulla. ",
+  },
+  {
+    id: 3,
+    image:
+      "https://www.sciencenews.org/wp-content/uploads/2020/02/021520_catallergies_main-1028x579.jpg",
+    name: "peter smith",
+    title: "product designer",
+    quote:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel lacinia felis. Vivamus vestibulum, libero a placerat semper, nisi tortor dictum velit, egestas iaculis felis libero quis nulla. ",
+  },
+  {
+    id: 4,
+    image:
+      "https://www.telegraph.co.uk/content/dam/Pets/spark/royal-canin/cat-close-to-screen.jpg",
+    name: "susan andersen",
+    title: "the boss",
+    quote:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel lacinia felis. Vivamus vestibulum, libero a placerat semper, nisi tortor dictum velit, egestas iaculis felis libero quis nulla. ",
+  },
+];
+
+export default people;
