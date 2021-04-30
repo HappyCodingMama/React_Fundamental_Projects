a2b93e00a6c904d1daa2362a230130df636b7267
HappyCodingMama <happycodingmama@gmail.com>
2021-04-30 22:40:33 +0900

    install react icons

--- 007reviews/src/App.js
+++ 007reviews/src/App.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
 import data from "./data";
 import "./App.css";
 
