99d4d10acaab7f90bf4e85190a24ee1ce1922d25
HappyCodingMama <happycodingmama@gmail.com>
2021-05-05 21:26:28 +0900

    Update App.js

--- 007reviews/src/App.js
+++ 007reviews/src/App.js
@@ -22,11 +22,14 @@ function App() {
           if (personIndex === index) {
             position = "activeSlide";
           }
-          if (personIndex === index - 1) {
-            position = "lastIndex";
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = "lastSlide";
           }
           return (
-            <article key={id}>
+            <article className={position} key={id}>
               <img src={image} alt={name} className="person-img" />
               <h4>{name}</h4>
               <p className="title">{title}</p>
