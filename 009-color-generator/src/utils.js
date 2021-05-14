054bdd113326c36798d54ad4a2a26852516fb809
HappyCodingMama <happycodingmama@gmail.com>
2021-05-15 08:37:28 +0900

    Create utils.js

---
+++ 009-color-generator/src/utils.js
@@ -0,0 +1,10 @@
+function componenToHex(c) {
+  var hex = c.String(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+export default rgbToHex;
