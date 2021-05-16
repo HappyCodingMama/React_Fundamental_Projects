c667f637ed5e4c05c7c56a017682a08f7f0e6d78
HappyCodingMama <happycodingmama@gmail.com>
2021-05-16 21:47:57 +0900

    update values.js

--- 009-color-generator/src/App.js
+++ 009-color-generator/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SingleColor from "./SingleColor";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Values from "values.js";
+
+const App = () => {
+  const [color, setColor] = useState("");
+
+  return <h2>color generator setup</h2>;
+};
 
 export default App;
