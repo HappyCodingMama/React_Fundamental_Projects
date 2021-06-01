d29158247207cedcc48477dc22fc398eb959fce3
HappyCodingMama <happycodingmama@gmail.com>
2021-06-01 21:58:37 +0900

    Update App.js

--- 010-grocery-bud/src/App.js
+++ 010-grocery-bud/src/App.js
@@ -9,8 +9,19 @@ function App() {
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
-  return <h2 className="App">grocery bud setup</h2>;
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}></form>
+      <div className="grocery-container">
+        <List />
+        <button className="clear-btn">clear items</button>
+      </div>
+    </section>
+  );
 }
 
 export default App;
