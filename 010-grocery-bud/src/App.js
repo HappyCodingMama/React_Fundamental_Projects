4ee1fc53785053b7f482bd79ca1ed7c4525149c2
HappyCodingMama <67956944+HappyCodingMama@users.noreply.github.com>
2021-06-05 21:17:12 +0900

    Update App.js

--- 010-grocery-bud/src/App.js
+++ 010-grocery-bud/src/App.js
@@ -1,61 +1,66 @@
-import React, { useState, useEffect } from "react";
-import List from "./List";
-import Alert from "./Alert";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import List from './List';
+import Alert from './Alert';
+import './App.css';
 
 function App() {
-  const [name, setName] = useState("");
+  const [name, setName] = useState('');
   const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({
     show: false,
-    msg: "",
-    type: "",
+    msg: '',
+    type: '',
   });
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!name) {
-      setAlert(true, "please enter value", "danger");
+      setAlert(true, 'please enter value', 'danger');
     } else if (name && isEditing) {
     } else {
-      showAlert(true, "success", "item added to the list");
+      showAlert(true, 'success', 'item added to the list');
       const newItem = { id: new Date().getTime().toString(), title: name };
       setList([...list, newItem]);
-      setName("");
+      setName('');
     }
   };
 
-  const showAlert = (show = false, type = "", msg = "") => {
+  const showAlert = (show = false, type = '', msg = '') => {
     setAlert({ show, type, msg });
   };
   const clearList = () => {
-    showAlert(true, "danger", "empty list");
+    showAlert(true, 'danger', 'empty list');
     setList([]);
   };
 
+  const removeItem = (id) => {
+    showAlert(true, 'danger', 'item removed');
+    setList(list.filter((item = item.id !== id)));
+  };
+
   return (
-    <section className="section-center">
-      <form className="grocery-form" onSubmit={handleSubmit}>
+    <section className='section-center'>
+      <form className='grocery-form' onSubmit={handleSubmit}>
         {alert.show && <Alert {...alert} removeAlert={showAlert} />}
         <h3>grocery bud</h3>
-        <div className="form-control">
+        <div className='form-control'>
           <input
-            type="text"
-            className="grocery"
-            placeholder="e.g. eggs"
+            type='text'
+            className='grocery'
+            placeholder='e.g. eggs'
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
-          <button type="submit" className="submit-btn">
-            {isEditing ? "edit" : "submit"}
+          <button type='submit' className='submit-btn'>
+            {isEditing ? 'edit' : 'submit'}
           </button>
         </div>
       </form>
       {list.length > 0 && (
-        <div className="grocery-container">
+        <div className='grocery-container'>
           <List items={list} />
-          <button className="clear-btn" onClick={clearList}>
+          <button className='clear-btn' onClick={clearList}>
             clear items
           </button>
         </div>
