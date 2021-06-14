6f8e1690ae7c9f74f6a8c461df22952c48566cd1
HappyCodingMama <happycodingmama@gmail.com>
2021-06-14 21:41:18 +0900

    Update data.js

--- 011-navbar/src/data.js
+++ 011-navbar/src/data.js
@@ -8,4 +8,25 @@ export const links = [
   { id: 5, url: "/profile", text: "profile" },
 ];
 
-export const social = [];
+export const social = [
+  {
+    id: 1,
+    url: "https://www.facebook.com",
+    icon: <FaFacebook />,
+  },
+  {
+    id: 2,
+    url: "https://www.twitter.com",
+    icon: <FaTwitter />,
+  },
+  {
+    id: 3,
+    url: "https://www.linkedin.com",
+    icon: <FaLinkedin />,
+  },
+  {
+    id: 4,
+    url: "https://www.behance.com",
+    icon: <FaBehance />,
+  },
+];
