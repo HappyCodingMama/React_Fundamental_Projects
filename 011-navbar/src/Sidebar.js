6b19102f282d64c9b462fb8d2ac827caff140dba
HappyCodingMama <happycodingmama@gmail.com>
2021-06-21 21:05:21 +0900

    Update Sidebar.js

--- 011-navbar/src/Sidebar.js
+++ 011-navbar/src/Sidebar.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Sidebar = () => {
-  return <div></div>;
+  return (
+    <ul className="links">
+      <li>
+        <a href="#">home</a>
+      </li>
+      <li>
+        <a href="#">about</a>
+      </li>
+      <li>
+        <a href="#">contact</a>
+      </li>
+      <li>
+        <a href="#">products</a>
+      </li>
+    </ul>
+  );
 };
 
 export default Sidebar;
