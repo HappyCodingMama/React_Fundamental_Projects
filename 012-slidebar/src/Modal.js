298560e1fd90aa5bf7c69363a7c0cee0df1130ab
HappyCodingMama <happycodingmama@gmail.com>
2021-07-08 21:45:48 +0900

    Update Modal.js

--- 012-slidebar/src/Modal.js
+++ 012-slidebar/src/Modal.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { FaTimes } from "react-icons/fa";
 
 const Modal = () => {
-  return <div>Modal</div>;
+  return (
+    <div className={`modal-overlay show-modal`}>
+      <div className="moda-container">
+        <h3>modal content</h3>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
