d4ebcc8d5c37f8d26da980cd81126e76a5b03fcf
HappyCodingMama <happycodingmama@gmail.com>
2021-07-12 13:18:00 +0900

    Create data.js

---
+++ 012-slidebar/src/data.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {
+  Fabehance,
+  FaFacebook,
+  FaLinkedin,
+  FaTwitter,
+  FaSketch,
+  FaHome,
+  FaUserFriends,
+  FaFolderOpen,
+  FaCalendarAlt,
+  FaWpforms,
+} from "React-icons/fa";
+
+export const links = [
+  {
+    id: 1,
+    url: "/",
+    text: "home",
+    icon: <FaHome />,
+  },
+];
