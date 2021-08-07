d590ea1739a990d772ed7591e6eb9014e49e2bec
HappyCodingMama <happycodingmama@gmail.com>
2021-08-07 15:54:04 +0900

    Update context.js

--- 012-slidebar/src/context.js
+++ 012-slidebar/src/context.js
@@ -7,4 +7,8 @@ const AppProvider = ({ children }) => {
   <AppContext.Provider value="hello">{children}</AppContext.Provider>;
 };
 
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
 export { AppContext, AppProvider };
