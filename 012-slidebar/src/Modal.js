125737a5b9ce3881402de55202673523c1d242f5
HappyCodingMama <happycodingmama@gmail.com>
2021-08-19 19:29:40 +0900

    Update Modal.js

--- 012-slidebar/src/Modal.js
+++ 012-slidebar/src/Modal.js
@@ -3,6 +3,7 @@ import { FaTimes } from "react-icons/fa";
 import { useGlobalContext } from "./context";
 
 const Modal = () => {
+  const { isModalOpen, closeModal } = useGlobalContext();
   return (
     <div className={`modal-overlay show-modal`}>
       <div className="moda-container">
