91ae3dd7460e761154ad6e67ad8d261791c41ff3
HappyCodingMama <happycodingmama@gmail.com>
2021-08-20 20:13:35 +0900

    Update Modal.js

--- 012-slidebar/src/Modal.js
+++ 012-slidebar/src/Modal.js
@@ -5,8 +5,12 @@ import { useGlobalContext } from "./context";
 const Modal = () => {
   const { isModalOpen, closeModal } = useGlobalContext();
   return (
-    <div className={`modal-overlay show-modal`}>
-      <div className="moda-container">
+    <div
+      className={`${
+        isModalOpen ? "modal-overlay show-modal" : "modal-overlay"
+      }`}
+    >
+      <div className="modal-container">
         <h3>modal content</h3>
         <button className="close-modal-btn">
           <FaTimes />
