77c930627916bde53858a91bdde1b935812d1785
HappyCodingMama <happycodingmama@gmail.com>
2021-08-30 21:09:24 +0900

    Update data.js

--- 013-stripe/src/data.js
+++ 013-stripe/src/data.js
@@ -1,2 +1,16 @@
 import { FaCreditCard, FaBook, FaBriefcase } from "react-icons/fa";
 import React from "react";
+const sublinks = [
+  {
+    page: "products",
+    links: [{ label: "payment", icon: <FaCreditCard />, url: "/products" }],
+  },
+  {
+    page: "terminal",
+    links: [{ label: "payment", icon: <FaCreditCard />, url: "/products" }],
+  },
+  {
+    page: "connect",
+    links: [{ label: "payment", icon: <FaCreditCard />, url: "/products" }],
+  },
+];
