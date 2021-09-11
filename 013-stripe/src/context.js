196c4060059f5185b254a711d29051a90b803c88
HappyCodingMama <happycodingmama@gmail.com>
2021-09-11 09:42:33 +0900

    Update context.js

--- 013-stripe/src/context.js
+++ 013-stripe/src/context.js
@@ -2,3 +2,7 @@ import React, { useState, useContext } from "react";
 import sublinks from "./data";
 
 const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  return <AppContext.Provider>{children}</AppContext.Provider>;
+};
