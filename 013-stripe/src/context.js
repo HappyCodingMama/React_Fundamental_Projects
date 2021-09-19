e5f34c77cfe82ab03fd2e20e3ebcd9592f6972dd
HappyCodingMama <happycodingmama@gmail.com>
2021-09-19 21:31:35 +0900

    Update context.js

--- 013-stripe/src/context.js
+++ 013-stripe/src/context.js
@@ -5,7 +5,7 @@ const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-  const [isModalOpen, setIsModalOpen] = useState(true);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
 
   const openSidebar = () => {
     setIsSidebarOpen(true);
